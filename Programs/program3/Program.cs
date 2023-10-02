147dc166648f3f35e20b5327ec4589fa22f8fde4
Alexey <>
2023-10-02 14:45:33 +0300

    Decided third program

--- Programs/program3/Program.cs
+++ Programs/program3/Program.cs
@@ -6,7 +6,24 @@ namespace program3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+            //1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+            //6, 1, 33 -> [6, 1, 33]
+
+            void ShowArray(int arraySize){
+                Console.Write("[");
+                int[] array = new int[arraySize];
+                for (int i = 0; i < array.Length; i++){
+                    array[i] = new Random().Next(1,10);
+                    Console.Write($"{array[i]}, ");
+                }
+                Console.Write("]");
+            }
+
+            
+            Console.WriteLine("Введите длину массива :");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            ShowArray(arraySize);
         }
     }
 }
